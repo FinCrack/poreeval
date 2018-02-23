88edb4c731b00a99fd6c6c6460550ea6ace2f4ca
SaubererBoii <janni@Jannik.fritz.box>
2018-02-23 21:44:30 +0100

    Loginservlet is nun auch da

--- Poreeval/src/controllers/LoginServlet.java
+++ Poreeval/src/controllers/LoginServlet.java
@@ -35,8 +35,9 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 	}
 
 }
