7fac53c151fde0affb71139c2bbfcfb15874b142
Grazzle <chris.lindenberg@web.de>
2018-02-25 17:45:23 +0100

    Usergedönse analog zum Product

--- Poreeval/src/models/UserModel.java
+++ Poreeval/src/models/UserModel.java
@@ -1,5 +1,22 @@
 package models;
 
+import java.sql.SQLException;
+
+import data.User;
+import dataAccess.UserDataAccess;
+
 public class UserModel {
 
+	private UserDataAccess userDataAccess = new UserDataAccess();
+
+	public void CreateUser(int id, String username, String email, String role, String password) {
+
+		User user = new User(id, username, email, role, password);
+
+		try {
+			this.userDataAccess.InsertUser(user);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
