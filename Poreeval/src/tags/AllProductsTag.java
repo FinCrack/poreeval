6146f7c706d388cee9ad524a01bb69929620adfd
SaubererBoii <janni@Jannik.fritz.box>
2018-02-27 11:51:12 +0100

    AllProductsTag.java mit TODO versehen. Einmal checken ob das überhaupt so realisierbar ist!

--- Poreeval/src/tags/AllProductsTag.java
+++ Poreeval/src/tags/AllProductsTag.java
@@ -1,12 +1,22 @@
 package tags;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class AllProductsTag extends TagSupport{
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;;
+	
+	//TODO Tabelle mit diesen Daten über Schleifen bilden! Tabelle mit allen Produkten!
+//	
+//	long ean = (long) pageContext.getRequest().getAttribute("ean");
+//	String name = (String) pageContext.getRequest().getAttribute("name");
+//	String description = (String) pageContext.getRequest().getAttribute("description");
+//	BufferedImage picture = (BufferedImage) pageContext.getRequest().getAttribute("picture");
+	
+	
 	
 	public int doStartTag() {
 		try {
