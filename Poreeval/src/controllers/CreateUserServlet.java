f0202d76079ec6c973f038fc47433a4e60dd50b7
Grazzle <chris.lindenberg@web.de>
2018-03-01 15:16:08 +0100

    Servlet fuer User und Login unnoetige imports gelöscht, poreeval sql roletable hinzugefuegt und dies auch in pgadmin

--- Poreeval/src/controllers/CreateUserServlet.java
+++ Poreeval/src/controllers/CreateUserServlet.java
@@ -2,7 +2,6 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 
 import javax.servlet.ServletException;
@@ -11,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import data.User;
 import models.UserModel;
 
 /**
--- Poreeval/src/controllers/LoginServlet.java
+++ Poreeval/src/controllers/LoginServlet.java
@@ -1,20 +1,14 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import data.User;
-import dataAccess.DatabaseConnection;
 import models.UserModel;
 
 /**
