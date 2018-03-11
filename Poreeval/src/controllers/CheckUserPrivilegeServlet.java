962fa4f95a134e4708002001aca98e7b1d967fec
Grazzle <chris.lindenberg@web.de>
2018-03-11 23:22:26 +0100

    Ansatz CheckUserPrivilegeServlet

--- Poreeval/src/controllers/CheckUserPrivilegeServlet.java
+++ Poreeval/src/controllers/CheckUserPrivilegeServlet.java
@@ -6,6 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import data.User;
 
 /**
  * Servlet implementation class CheckUserPrivilegeServlet
@@ -13,29 +16,44 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CheckUserPrivilegeServlet")
 public class CheckUserPrivilegeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CheckUserPrivilegeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CheckUserPrivilegeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+
+		switch (user.getPrivilege()) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+		default:
+			break;
+		}
 	}
 
 }
