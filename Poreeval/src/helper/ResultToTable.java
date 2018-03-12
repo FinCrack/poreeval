d93cafbf78bb36dbf72e9d55aab248798ea1e257
SaubererBoii <janni@Jannik.fritz.box>
2018-03-12 20:36:35 +0100

    ShowProductDetails.jsp angelegt, aus den Tabellen kann darauf verwiesen werden allerdings erstmal auf die allgemeine Seite und nicht auf das spezifische Produkt

--- Poreeval/src/helper/ResultToTable.java
+++ Poreeval/src/helper/ResultToTable.java
@@ -36,14 +36,14 @@ public class ResultToTable {
 		return table;
 	}
 	
-	
+	// TODO 
 	public static String ToTable(List<Product> products) {
 		String table = "";
 		for (Product product : products) {
 			table += "<div class='container'>";
 			table += "<table class='table table-striped tabled-bordered table-hover table-condensed'>";
 			table += "<td rowspan='3' >TODO Bild</td>";
-			table += "<td class='success'>" + product.getProductname() + "</td>";
+			table += "<td><a href='showProductDetails.jsp' class='list-group-item active'>" + product.getProductname() + "</a></td>";
 			table += "<tr><td>" + product.getDescription() + "</td></tr>";
 			table += "<tr><td>" + product.getRating() + "</td></tr>";
 			table += "</table>";
@@ -53,35 +53,3 @@ public class ResultToTable {
 	}
 }
 
-
-// Zu verbessernde Variante, TD in der der Produktname steht, soll oncklick die Detailansicht eines Produkts anzeigen
-// 
-//
-//
-//	public static String ToTable(List<Product> products) {
-//		String table = "";
-//		for (Product product : products) {
-//			table += "<div class='container'>";
-//			table += "<table class='table table-striped tabled-bordered table-hover table-condensed'>";
-//			table += "<td rowspan='3' >TODO Bild</td>";
-//	
-//			table += "<td class='success' onclick='showProductDetails(this)'>" + product.getProductname() + "</td>";
-//
-//			table += "<tr><td>" + product.getDescription() + "</td></tr>";
-//			table += "<tr><td>" + product.getRating() + "</td></tr>";
-//			table += "</table>";
-//			table += "</div>";
-//		}
-//		return table;
-//	}
-//	
-//	<script>
-//		function showProductDetails() {
-//			window.location.href = "showProductDetails.jsp"
-//			
-//		}
-//		
-//  </script>
-//	
-//	
-//	
