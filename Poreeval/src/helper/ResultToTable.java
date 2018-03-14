14bcf5edec10c2737bb9d5a22a5c7cc0bb0ce274
SaubererBoii <janni@Jannik.fritz.box>
2018-03-14 15:28:38 +0100

    kleine änderungen

--- Poreeval/src/helper/ResultToTable.java
+++ Poreeval/src/helper/ResultToTable.java
@@ -1,8 +1,5 @@
 package helper;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
 import java.util.List;
 
 import data.Product;
@@ -13,36 +10,12 @@ import data.Product;
 
 public class ResultToTable {
 
-	public static String ToTable(ResultSet rs) {
-		String table = "<table>";
-		try {
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int col = rsmd.getColumnCount();
-			table += "<tr>";
-			for (int i = 1; i != col; i++) {
-				table += "<th>" + rsmd.getColumnName(i) + "</th>";
-			}
-			table += "<tr>";
-			do {
-				table += "<tr>";
-				for (int i = 1; i != col; i++) {
-					table += "<td>" + rs.getString(i) + "</td>";
-				}
-				table += "</tr>";
-			} while (rs.next());
-		} catch (SQLException e) {
-			table = "Keine Ergebnisse";
-		}
-		return table;
-	}
-	
-	// TODO 
 	public static String ToTable(List<Product> products) {
 		String table = "";
 		for (Product product : products) {
 			table += "<div class='container'>";
 			table += "<table class='table table-striped tabled-bordered table-hover table-condensed'>";
-			table += "<td rowspan='3' >TODO Bild</td>";
+			table += "<td rowspan='3'><img src='https://www.spirulina.pl/wp-content/uploads/2015/07/naturalny-produkt.jpg'/></td>";
 			table += "<td><a href='showProductDetails.jsp' class='list-group-item active'>" + product.getProductname() + "</a></td>";
 			table += "<tr><td>" + product.getDescription() + "</td></tr>";
 			table += "<tr><td>" + product.getRating() + "</td></tr>";
@@ -52,4 +25,3 @@ public class ResultToTable {
 		return table;
 	}
 }
-
--- Poreeval/src/tags/NavigationTag.java
+++ Poreeval/src/tags/NavigationTag.java
@@ -74,8 +74,7 @@ public class NavigationTag extends TagSupport {
 							+ "</div>"
 						+ "</div>"	
 				+ "</nav>"
-				+ "<script "
-				+ "src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>"
+				+ "<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script>"
 				+ "<script src='js/bootstrap.min.js'></script>";
 		return navbar;
 	}
