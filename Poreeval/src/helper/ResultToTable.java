47980485d55b61c7a29110f4ed60377ba8dc0053
SaubererBoii <janni@Jannik.fritz.box>
2018-03-14 15:35:05 +0100

    Änderungen css

--- Poreeval/src/helper/ResultToTable.java
+++ Poreeval/src/helper/ResultToTable.java
@@ -13,14 +13,12 @@ public class ResultToTable {
 	public static String ToTable(List<Product> products) {
 		String table = "";
 		for (Product product : products) {
-			table += "<div class='container'>";
 			table += "<table class='table table-striped tabled-bordered table-hover table-condensed'>";
 			table += "<td rowspan='3'><img src='https://www.spirulina.pl/wp-content/uploads/2015/07/naturalny-produkt.jpg'/></td>";
 			table += "<td><a href='showProductDetails.jsp' class='list-group-item active'>" + product.getProductname() + "</a></td>";
 			table += "<tr><td>" + product.getDescription() + "</td></tr>";
 			table += "<tr><td>" + product.getRating() + "</td></tr>";
 			table += "</table>";
-			table += "</div>";
 		}
 		return table;
 	}
