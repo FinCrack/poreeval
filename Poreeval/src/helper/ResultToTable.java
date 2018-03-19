22cfe8d0645b68443387485d48a253e1c1d908a1
SaubererBoii <janni@Jannik.fritz.box>
2018-03-19 19:29:32 +0100

    ResultToTable zeigt alles an was gewünscht ist, Formatierung fehlt!

--- Poreeval/src/helper/ResultToTable.java
+++ Poreeval/src/helper/ResultToTable.java
@@ -28,24 +28,27 @@ public class ResultToTable {
 	}
 	
 	public static String ProductWithReviewsToTable(Product product){
-		String table= "";
+		String table = "";
 		
 		//Table der das Produkt anzeigt
 			table += "<p><img src='https://www.spirulina.pl/wp-content/uploads/2015/07/naturalny-produkt.jpg' width='200' height='200' /></p>";
-			table += "<table class='table table-striped tabled-bordered table-hover table-condensed'>";
-				table += "<tr>";
+			table += "<table class='table table-striped tabled-bordered table-condensed' style='margin-bottom: 100px;'>";
+			table += GetEditForm(product);
+			table += "<tr>";
 					table += "<td>" + product.getProductname() + "</td>";
 				table += "</tr>";
+
 				table += "<tr>";
 					table += "<td>" + GetStars(product) + "</td>";
 				table += "</tr>";
 			table += "</table>";
 			
 			//Table der die Reviews zu einem Produkt anzeigt
+			table += "<h4>Kundenrezensionen:</h4>";
 			for(Review review : product.getReviews()){
-			table += "<table>";
+			table += "<table class='table table-striped tabled-bordered table-condensed'>";
 				table += "<tr>";
-					table += "<td>" + review.getRating() +  "</td><td>" + review.getTitle() + "</td>";
+					table += "<td>" + GetStars(product) +  "</td><td>" + review.getTitle() + "</td>";
 				table += "</tr>";
 					table += "<td>" + review.getUser_name() + "</td><td>" + review.getReview_date() + "</td>";
 				table += "</tr>";
@@ -78,31 +81,12 @@ public class ResultToTable {
 		return imageCont;
 	}
 	
-	/*
-	 * <p><img src="https://www.spirulina.pl/wp-content/uploads/2015/07/naturalny-produkt.jpg" width="135" height="135" /></p>
-<table class="table table-striped tabled-bordered table-hover table-condensed" style="width: 257px; height: 78px;">
-<tbody>
-<tr>
-<td style="width: 247px;">Name</td>
-</tr>
-<tr>
-<td style="width: 247px;">Durchschnittliches Rating</td>
-</tr>
-</tbody>
-</table>
-<table>
-<tbody>
-<tr>
-<td>Rating</td>
-<td>&Uuml;berschrift</td>
-</tr>
-<tr>
-<td>Username</td>
-<td>Date</td>
-</tr>
-</tbody>
-</table>
-<p>text</p>
-	 * */
+	public static String GetEditForm(Product product){
+		String editForm = "<form action='EditProductServlet' method='post'>"
+				+ "<input type='hidden' name='ean' value='" + product.getEan() + "'>"
+				+ "<button type='submit' class='btn btn-primary'>Produckt bearbeiten</button>"
+				+ "</form>";
+		return editForm;
+	}
 }
 
